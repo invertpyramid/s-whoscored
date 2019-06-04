669636ff193e154e70401c4625232708496adfdb
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-04 23:19:27 +1000

    Add a setting for WhoScored

---
+++ s_whoscored/settings/whoscored.py
@@ -0,0 +1,7 @@
+REGIONS = {
+    (252, 2),  # England, Premier League
+    (108, 5),  # Italy, Serie A
+    (206, 4),  # Spain, La Liga
+    (81, 3),  # Germany, Bundesliga
+    (74, 38),  # France, Ligue 1
+}
