5c6cf5fa50c4b28047a0e9f96fb75053690a43c3
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-05 18:44:41 +1000

    Update httpcache configuration
    This commit updates the configuration of httpcache, and the relative
    docker configuration is in the foler of docker/Percona-Server-MongoDB

--- s_whoscored/settings/httpcache.py
+++ s_whoscored/settings/httpcache.py
@@ -1,8 +1,28 @@
 """
 httpcache configuration for this spider
 """
+from s_whoscored.settings import DOWNLOADER_MIDDLEWARES
+
+DOWNLOADER_MIDDLEWARES.update(
+    {
+        "scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware": None,
+        "scrapy_httpcache.downloadermiddlewares.httpcache.AsyncHttpCacheMiddleware": 900,
+    }
+)
+
 HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_IGNORE_HTTP_CODES = [301, 302, 500, 503]
+
+HTTPCACHE_STORAGE = "scrapy_httpcache.extensions.httpcache_storage.MongoDBCacheStorage"
+
+HTTPCACHE_MONGODB_HOST = "127.0.0.1"
+HTTPCACHE_MONGODB_PORT = 27019
+
+HTTPCACHE_MONGODB_USERNAME = "root"
+HTTPCACHE_MONGODB_PASSWORD = "xxxxxxxx"
+HTTPCACHE_MONGODB_AUTH_DB = "admin"
+
+HTTPCACHE_MONGODB_CONNECTION_POOL_KWARGS = {}
+
+HTTPCACHE_MONGODB_DB = "cache_storage"
+HTTPCACHE_MONGODB_COLL = "cache"
