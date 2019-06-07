828c4affa909ad316efba289eb4c67673e22bd02
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-07 15:09:33 +1000

    Reformat downloadermiddlewares to fit pylint requirements

--- s_whoscored/downloadermiddlewares/__init__.py
+++ s_whoscored/downloadermiddlewares/__init__.py
@@ -1,3 +1,6 @@
+"""
+The downloadermiddlewares module developed only for this spider
+"""
 import functools
 from asyncio.events import get_event_loop
 from asyncio.futures import Future
@@ -10,6 +13,14 @@ from twisted.internet.defer import Deferred
 
 
 def as_deferred(func) -> Callable[..., Deferred]:
+    """
+    Convert Future to Deferred
+    :param func:
+    :type func: Callable
+    :return:
+    :type: Callable[..., Deferred]
+    """
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         return Deferred.fromFuture(ensure_future(func(*args, **kwargs)))
@@ -17,11 +28,25 @@ def as_deferred(func) -> Callable[..., Deferred]:
     return wrapper
 
 
-def as_future(d: Deferred) -> Future:
-    return d.asFuture(get_event_loop())
+def as_future(deferred: Deferred) -> Future:
+    """
+    Convert Deferred to Future
+    :param deferred:
+    :type deferred: Deferred
+    :return:
+    :type: Future
+    """
+    return deferred.asFuture(get_event_loop())
 
 
 async def validate_response(response: Union[Response, Page]) -> bool:
+    """
+    Validate the response by its content
+    :param response:
+    :type response: Union[Response, Page]
+    :return:
+    :rtype: bool
+    """
     if isinstance(response, Page):
         content: str = await response.content()
         body: bytes = content.encode()
--- s_whoscored/downloadermiddlewares/block_inspector.py
+++ s_whoscored/downloadermiddlewares/block_inspector.py
@@ -1,5 +1,5 @@
 """
-A middleware for block inspection
+A middleware for block inspection by response
 """
 
 from scrapy.crawler import Crawler
@@ -12,21 +12,32 @@ from s_whoscored.downloadermiddlewares import as_deferred, validate_response
 
 
 class BlockInspectorMiddleware:
+    """
+    Block inspection by response
+    """
+
     def __init__(self, crawler: Crawler):
         self.crawler: Crawler = crawler
         self.settings: Settings = crawler.settings
 
     @classmethod
     def from_crawler(cls, crawler: Crawler):
+        """
+        Initial a middleware instance with crawler
+        :param crawler:
+        :type crawler: Crawler
+        :return:
+        :rtype: BlockInspectorMiddleware
+        """
         obj = cls(crawler=crawler)
         return obj
 
     @as_deferred
     async def process_response(
-        self, response: Response, request: Request, spider: Spider
+            self, response: Response, request: Request, spider: Spider
     ) -> Response:
         """
-
+        Inspect block by the content of response
         :param request:
         :type request: Request
         :param response:
@@ -34,9 +45,8 @@ class BlockInspectorMiddleware:
         :param spider:
         :type spider: Spider
         :return:
-        :rtype:
+        :rtype: Response
         """
         if await validate_response(response):
             return response
-        else:
-            raise IgnoreRequest
+        raise IgnoreRequest
