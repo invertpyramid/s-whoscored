49732fccffa802a7e83b779c81825f270b744f86
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-07 13:56:42 +1000

    Black and pylint the spider module
    This commit uses black and pylint on the spider module to increase the
    code quality

--- s_whoscored/spiders/whoscored_statistics.py
+++ s_whoscored/spiders/whoscored_statistics.py
@@ -1,39 +1,44 @@
-import pprint
+"""
+The spider scrapes WhoScored Statistics page
+"""
 import re
+from typing import Generator
 
 from py_mini_racer.py_mini_racer import MiniRacer
-from scrapy.http import Response
+from scrapy.http import Request, Response
 from scrapy.spiders import Spider
 
-pp = pprint.PrettyPrinter(indent=4, width=120)
-
-p_tournaments = re.compile(
-    r"var\sallRegions\s=\s(?P<tournaments>.+?);", flags=re.DOTALL
-)
+P_TOUR = re.compile(r"var\sallRegions\s=\s(?P<tournaments>.+?);", flags=re.DOTALL)
 
 
 class WhoScoredStatisticsSpider(Spider):
+    """
+    A spider inheriting the generic spider of Scrapy and scraping WhoScored
+    Statistics page
+    """
+
     name = "WhoScored Statistic"
     start_urls = ["https://www.whoscored.com/Statistics"]
 
-    def parse(self, response: Response):
+    def parse(self, response: Response) -> Generator[Request, None, None]:
         """
-        Parse whoscored statistic page to get all tournaments url, e.g. Premier
-        League (England) as default
+        Parse whoscored statistic page to get all tournaments url
         :param response:
         :return:
 
+        # Scrapy check - because of settings missing, use Premier League
+        # (England) only for test purpose
         @url https://www.whoscored.com/Statistics
         @returns requests 1
         """
         js_script = response.css("#layout-wrapper > script::text").extract_first()
-        tournaments = p_tournaments.search(js_script).group("tournaments")
-        ctx = MiniRacer()
+        tournaments = P_TOUR.search(js_script).group("tournaments")
 
+        ctx = MiniRacer()
         for region in ctx.eval(tournaments):
             for tournament in filter(lambda x: x["name"], region["tournaments"]):
                 if (region["id"], tournament["id"]) in self.settings.get(
-                    "REGIONS", {(252, 2)}  # England, Premier League (as default)
+                        "REGIONS", {(252, 2)}  # England, Premier League (as default)
                 ):
                     yield response.follow(
                         tournament["url"], callback=self.parse_tournaments
@@ -46,6 +51,6 @@ class WhoScoredStatisticsSpider(Spider):
         :return:
 
         @url https://www.whoscored.com/Regions/252/Tournaments/2/England-Premier-League
-        @returns requests 1
+        @returns requests 0
         """
         pass
