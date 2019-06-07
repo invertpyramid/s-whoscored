8ab2404eac2d6c85353ba541ba0d2e452896852f
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-07 14:34:16 +1000

    Remove unused codes in items.py and pipelines.py

--- s_whoscored/items.py
+++ s_whoscored/items.py
@@ -1,11 +1,3 @@
 """
 The parsers used in ItemLoader and Item's processors
 """
-
-from scrapy.item import Item
-
-
-class WhoScoredItem(Item):
-    """
-    The item which store all data extracted from web pages
-    """
--- s_whoscored/pipelines.py
+++ s_whoscored/pipelines.py
@@ -1,23 +1,3 @@
 """
 the pipelines used in this project
 """
-from scrapy.item import Item
-from scrapy.spiders import Spider
-
-
-class WhoScoredPipeline:
-    """
-    the pipeline used in this project
-    """
-
-    def process_item(self, item: Item, spider: Spider) -> Item:
-        """
-
-        :param item:
-        :type item: Item
-        :param spider:
-        :type spider: Spider
-        :return:
-        :rtype: Item
-        """
-        return item
