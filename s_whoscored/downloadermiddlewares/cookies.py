2f9fdfe8af481c9ef7f16f0f4353fd8bbf020ae5
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-11 13:51:16 +1000

    Add a customized cookies downloader middleware
    This commit adds a customized empty cookies downloader middleware, which
    will deal with the cookies invalidation problem

---
+++ s_whoscored/downloadermiddlewares/cookies.py
@@ -0,0 +1,41 @@
+"""
+The customized cookies downloader middleware
+
+"""
+import logging
+from typing import Optional
+
+from pyppeteer.browser import Browser
+from pyppeteer.launcher import launch
+from scrapy.settings import Settings
+from scrapy.spiders import Spider
+from scrapy_cookies.downloadermiddlewares.cookies import CookiesMiddleware as CM_Origin
+
+from s_whoscored.crawler import Crawler
+from s_whoscored.downloadermiddlewares import as_deferred
+
+
+class CookiesMiddleware(CM_Origin):
+    """
+    The customized cookies downloader middleware
+    """
+    def __init__(self, settings: Settings):
+        super(CookiesMiddleware, self).__init__(settings=settings)
+        self.browser: Optional[Browser] = None
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler):
+        obj = super().from_crawler(crawler=crawler)
+        return obj
+
+    @as_deferred
+    async def spider_opened(self, signal: object, sender: Crawler, spider: Spider):
+        super(CookiesMiddleware, self).spider_opened(spider=spider)
+        self.browser = await launch(headless=False, logLevel=logging.WARNING)
+
+    @as_deferred
+    async def spider_closed(
+            self, signal: object, sender: Crawler, reason: str, spider: Spider
+    ):
+        super(CookiesMiddleware, self).spider_closed(spider=spider)
+        await self.browser.close()
--- s_whoscored/settings/cookies.py
+++ s_whoscored/settings/cookies.py
@@ -1,2 +1,15 @@
-# Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+"""
+middlewares configuration for this spider
+"""
+from s_whoscored.settings import DOWNLOADER_MIDDLEWARES
+
+DOWNLOADER_MIDDLEWARES.update(
+    {
+        "scrapy.downloadermiddlewares.cookies.CookiesMiddleware": None,
+        "s_whoscored.downloadermiddlewares.cookies.CookiesMiddleware": 700,
+    }
+)
+
+COOKIES_STORAGE = "scrapy_cookies.storage.mongo.MongoStorage"
+COOKIES_MONGO_MONGOCLIENT_HOST = "127.0.0.1"
+COOKIES_MONGO_MONGOCLIENT_PORT = 27020
