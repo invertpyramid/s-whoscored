1417cdb95a5106cc99064423515d1212881d9582
Grammy Jiang <gjiang@buildingsalive.com>
2019-12-10 17:17:33 +1100

    Remove chrome extension settings

--- s_whoscored/settings/chrome.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from s_whoscored.settings import EXTENSIONS
-
-EXTENSIONS.update({"s_whoscored.extensions.chrome.ChromeExtension": 0})
-
-BROWSER_LEVEL = logging.WARNING
-WS_P_LOG_LEVEL = logging.WARNING
-WS_C_LOG_LEVEL = logging.WARNING
