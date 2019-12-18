0003f44a0e8763981bce5e334f35508a8989828f
Grammy Jiang <gjiang@buildingsalive.com>
2019-12-18 13:34:06 +1100

    Make the block inspector middleware simple

--- s_whoscored/downloadermiddlewares/block_inspector.py
+++ s_whoscored/downloadermiddlewares/block_inspector.py
@@ -4,18 +4,13 @@ A middleware for block inspection by response
 from __future__ import annotations
 
 import logging
-from typing import List, Optional, Union
+from typing import List, Union
 
-from pyppeteer.browser import Browser
-from pyppeteer.launcher import launch
-from pyppeteer.page import Page
 from scrapy.http import Request, Response
 from scrapy.selector.unified import Selector
 from scrapy.settings import Settings
 from scrapy.signals import spider_closed, spider_opened
 from scrapy.spiders import Spider
-from websockets.client import logger as WS_C_Logger
-from websockets.protocol import logger as WS_P_Logger
 
 from s_whoscored.crawler import Crawler
 from s_whoscored.downloadermiddlewares import as_deferred
@@ -37,8 +32,6 @@ class BlockInspectorMiddleware:
         self.crawler: Crawler = crawler
         self.settings: Settings = crawler.settings
 
-        self.browser: Optional[Browser] = None
-
     @classmethod
     def from_crawler(cls, crawler: Crawler) -> BlockInspectorMiddleware:
         """
@@ -52,9 +45,6 @@ class BlockInspectorMiddleware:
         crawler.signals.connect(obj.spider_opened, spider_opened)
         crawler.signals.connect(obj.spider_closed, spider_closed)
 
-        WS_C_Logger.setLevel(crawler.settings["WS_C_LOG_LEVEL"])
-        WS_P_Logger.setLevel(crawler.settings["WS_P_LOG_LEVEL"])
-
         return obj
 
     @as_deferred
@@ -67,10 +57,6 @@ class BlockInspectorMiddleware:
         """
         logger.info("The middleware block inspector is up.")
 
-        self.browser: Browser = await launch(
-            headless=False, logLevel=self.settings["BROWSER_LEVEL"]
-        )
-
     @as_deferred
     async def spider_closed(self, spider: Spider, *args, **kwargs) -> None:
         """
@@ -80,27 +66,6 @@ class BlockInspectorMiddleware:
         :rtype: None
         """
         logger.info("The middleware block inspector is down.")
-
-        await self.browser.close()
-
-    async def _process_pyppeteer_response(  # pylint: disable=bad-continuation
-        self, response: Response, request: Request, spider: Spider
-    ) -> Response:
-        """
-        TODO: resolve the blocked response
-        :param response:
-        :type response: Response
-        :param request:
-        :type request: Request
-        :param spider:
-        :type spider: Spider
-        :return:
-        :rtype: Response
-        """
-        page: Page = await self.browser.newPage()
-        await page.close()
-
-        return response
 
     def _validate_response(self, response: Union[Response, str]) -> bool:
         """
@@ -120,7 +85,7 @@ class BlockInspectorMiddleware:
 
         return "ROBOTS" not in names_in_meta
 
-    async def process_response(  # pylint: disable=bad-continuation
+    def process_response(  # pylint: disable=bad-continuation
         self, response: Response, request: Request, spider: Spider
     ) -> Response:
         """
@@ -139,8 +104,6 @@ class BlockInspectorMiddleware:
 
         self.crawler.stats.inc_value("whoscored/response_blocked")
 
-        resp: Response = await self._process_pyppeteer_response(
-            response, request, spider
-        )
+        # TODO: fix the block
 
-        return resp
+        return response
--- s_whoscored/settings/block_inspector.py
+++ s_whoscored/settings/block_inspector.py
@@ -1,17 +1,8 @@
 """
 Block inspector middleware configuration
 """
-import logging
-
 from s_whoscored.settings import DOWNLOADER_MIDDLEWARES
 
 DOWNLOADER_MIDDLEWARES.update(
-    {
-        "s_whoscored.downloadermiddlewares.block_inspector.BlockInspectorMiddleware": 950,
-        "scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware": 960,
-    }
+    {"s_whoscored.downloadermiddlewares.block_inspector.BlockInspectorMiddleware": 90,}
 )
-
-BROWSER_LEVEL = logging.WARNING
-WS_C_LOG_LEVEL = logging.WARNING
-WS_P_LOG_LEVEL = logging.WARNING
